099d5da789ebd2199121caf71659fcc8f6bd8b3a
Lukasz <l.korkus1997@gmail.com>
2024-12-04 11:45:11 +0100

    style: 💄 Format and lint code in project
    Applied code formatting and linting for consistent code style across the
    project.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-				gray: {
-					200: '#EAECF0',
-					300: '#D0D5DD',
-					400: '#667085',
-					500: '#475467',
-					600: '#344054',
-					700: '#101828',
-				},
-				violet: {
-					400: '#7F56D9',
-					500: '#6941C6'
-				}
-			},
-			boxShadow: {
-				sm: ' 1px 2px  #1018280D'
-			},
+        gray: {
+          200: '#EAECF0',
+          300: '#D0D5DD',
+          400: '#667085',
+          500: '#475467',
+          600: '#344054',
+          700: '#101828',
+        },
+        violet: {
+          400: '#7F56D9',
+          500: '#6941C6',
+        },
+      },
+      boxShadow: {
+        sm: ' 1px 2px  #1018280D',
+      },
     },
   },
   plugins: [],
