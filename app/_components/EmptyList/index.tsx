80332715defe19e62a6658f3ea731aa016c1919c
Lukasz <l.korkus1997@gmail.com>
2024-12-05 14:16:21 +0100

    feat: 🎸 Add openForm method to EmptyList
    Handle openForm in EmptyList component to add first element

--- app/_components/EmptyList/index.tsx
+++ app/_components/EmptyList/index.tsx
@@ -1,13 +1,12 @@
-import { Dispatch, SetStateAction } from 'react';
-
 import { Button } from '@components/ui/Button';
 import { PlusIcon } from '@components/ui/Icons/PlusIcon';
 
 type EmptyListProps = {
-  setFormIsOpen: Dispatch<SetStateAction<boolean>>;
+  id: string;
+  openForm: (id: string) => void;
 };
 
-export const EmptyList = ({ setFormIsOpen }: EmptyListProps) => {
+export const EmptyList = ({ id, openForm }: EmptyListProps) => {
   return (
     <div className='flex flex-col items-center justify-center rounded-lg bg-gray-50 p-6'>
       <h1 className='text-center text-base font-semibold text-gray-700'>
@@ -16,7 +15,7 @@ export const EmptyList = ({ setFormIsOpen }: EmptyListProps) => {
       <p className='mt-1 text-center text-sm text-gray-500'>
         W tym menu nie ma jeszcze żadnych linków.
       </p>
-      <Button onClick={() => setFormIsOpen(true)} className='mt-6'>
+      <Button onClick={() => openForm(id)} className='mt-6'>
         <PlusIcon />
         Dodaj pozycję menu
       </Button>
