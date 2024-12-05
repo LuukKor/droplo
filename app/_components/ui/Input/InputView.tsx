6766b82585b3788ce50f7d5d4a802bd53c06eea9
Lukasz <l.korkus1997@gmail.com>
2024-12-05 18:53:27 +0100

    style: 💄 Add shadow to buttons and inputs

--- app/_components/ui/Input/InputView.tsx
+++ app/_components/ui/Input/InputView.tsx
@@ -30,7 +30,7 @@ export const InputView = ({
       </label>
       <input
         className={cn(
-          'pl- rounded-lg border border-gray-300 px-3 py-2 text-base font-normal text-gray-600 placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-violet-300',
+          'rounded-lg border border-gray-300 px-3 py-2 text-base font-normal text-gray-600 shadow-sm placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-violet-300',
           inputClassNames
         )}
         defaultValue={defaultValue}
